972d924a88bee86de496739b8ac18554368cc69e
cw <c2020w@163.com>
2020-09-01 10:50:00 +0800

    初始化了项目、并调整了项目目录结构

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入全局css样式
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
